8eab9112042d623d9f0d0d6248e47e37135b1d37
DK714 <dk150114@163.com>
2017-05-23 12:02:04 +0800

    界面……
    真的累……还没完全弄好……

---
+++ resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 RKDBMS1.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_RKDBMS1TYPE                 130
+#define ID_DATABASE_CREATEDATABASE      32771
+#define ID_DATABASE_CREATEDATABASE32772 32772
+#define ID_DATABASE_OPENDATABASE        32773
+#define ID_TABLE_NEWTABLE               32774
+#define ID_TABLE_MODIFYTABLE            32775
+#define ID_TABLE_OPENTABLE              32776
+#define ID_TABLE_DELETETABLE            32777
+#define ID_FIELD_ADDFIELD               32778
+#define ID_FIELD_MODIFYFIELD            32779
+#define ID_FIELD_DELETEFIELD            32780
+#define ID_RECORD_ADDRECORD             32781
+#define ID_RECORD_MODIFYRECORD          32782
+#define ID_RECORD_DELETERECORD          32783
+#define ID_HELP_ABOUTDATABASEMANAGEMENTSYSTEM 32784
+#define IDM_CREATE_DATABASE             32785
+#define IDM_OPEN_DATABASE               32786
+#define IDM_NEW_TABLE                   32787
+#define IDM_MODIFY_TABLE                32788
+#define IDM_OPEN_TABLE                  32789
+#define IDM_DELETE_TABLE                32790
+#define IDM_ADD_FIELD                   32791
+#define IDM_MODIFY_FIELD                32792
+#define IDM_DELETE_FIELD                32793
+#define IDM_ADD_RECORD                  32794
+#define IDM_MODIFY_RECORD               32795
+#define IDM_DELETE_RECORD               32796
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32797
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
